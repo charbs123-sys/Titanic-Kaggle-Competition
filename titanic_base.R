f7043010e4e665140ab5a77dfdfe39df4ab822c7
charbs123-sys <charbel.badr04@hotmail.com>
2024-09-03 14:35:20 +1000

    implementing cross validation and using caswise deletion with logistic regression, accuracy is used as the metric for comparison

--- titanic_base.R
+++ titanic_base.R
@@ -7,19 +7,45 @@ train <- read.csv("train.csv")
 #cleaning dataset
 train <- train %>% select(-PassengerId,-Name,-Ticket,-Cabin)
 n <- length(train$Cabin)
-#for (i in 1:n){
-#  if (train$Cabin[i] == ""){
-#    train$Cabin[i] = NA
-#  }
-#}
 
-train
+train <- train %>% na.omit()
+
 #splitting dataset
 splitting <- sample(1:nrow(train),size = 0.8 * nrow(train))
 tr <- train[splitting,]
 te <- train[-splitting,]
+tr_shuffle <- tr[sample(1:nrow(tr)),]
 
-#segmenting dependent and independent variables
+#cross validation
+
+cross_val <- function(k,tr_shuffle){
+#implementing k-fold CV
+fold_size <- floor(nrow(tr_shuffle)/k)
+class <- numeric(k)
+
+for(i in 1:k){
+  val_indices <- ((i-1) * fold_size + 1): (i * fold_size)
+  validation <- tr_shuffle[val_indices,]
+  training <- tr_shuffle[-val_indices,]
+  
+  logist_cv <- glm(Survived ~ ., data = training)
+  
+  pred <- ifelse(predict(logist_cv,newdata = validation, type = "response") > 0.5,1,0)
+  pred <- factor(pred)
+  test_pred <- factor(validation$Survived)
+  conf <- confusionMatrix(pred,test_pred)
+  class[i] <- conf$byClass[11]
+}
+
+#final metrics for comparison
+class_avg <- mean(class)
+return(class_avg)
+}
+
+accuracy_casewise <- cross_val(10,tr_shuffle)
+
+
+#implement model with highest accuracy
 y_tr <- factor(tr$Survived)
 x_tr <- tr %>% select(-Survived)
 
@@ -31,7 +57,9 @@ x_te <- te %>% select(-Survived)
 te_log <- predict(logist,x_te, type = "response")
 
 
-x_tr
-#newstuff-- this branch is new
 
-#new stuff for this branch
+#determining preformance against test data
+outcomes <- factor(ifelse(te_log > 0.5,1,0))
+y_te <- factor(te$Survived)
+(conf <- confusionMatrix(y_te,outcomes))
+
