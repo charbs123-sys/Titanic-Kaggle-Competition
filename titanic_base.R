94399db2ffa17c7aceefd089aa8f7c996f9d7aed
charbs123-sys <charbel.badr04@hotmail.com>
2024-09-24 15:18:34 +1000

    adding multiple machine learning model without parameter tuning

--- titanic_base.R
+++ titanic_base.R
@@ -1,11 +1,12 @@
 library(tidyverse)
 library(caret)
-
+library(MASS)
+library(e1071)
 #importing datasets
 train <- read.csv("train.csv")
 
 #cleaning dataset
-train <- train %>% select(-PassengerId,-Name,-Ticket,-Cabin)
+train <- train %>% dplyr::select(-PassengerId,-Name,-Ticket,-Cabin)
 n <- length(train$Cabin)
 train <- train[-which(train$Embarked == ""),]
 
@@ -37,8 +38,8 @@ train_NNA <- train_L %>% group_by(Survived,Sex)
 train_NNA <- train_NNA[!is.na(train_NNA$Age),]
 train_sum <- train_NNA %>% summarise(means = mean(Age))
 
-row_exist_NNA <- train_sum %>% select(Survived,Sex)
-row_exists_NA <- train_NA %>% select(Survived,Sex)
+row_exist_NNA <- train_sum %>% dplyr::select(Survived,Sex)
+row_exists_NA <- train_NA %>% dplyr::select(Survived,Sex)
 
 empt <- c()
 for (i in 1:nrow(row_exists_NA)){
@@ -47,7 +48,7 @@ for (i in 1:nrow(row_exists_NA)){
     if (bool[1] & bool[2]){
       empt <- c(empt,train_sum$means[j])
     } } }
-train_NA <- train_NA %>% select(-Age) %>% mutate(Age = empt)
+train_NA <- train_NA %>% dplyr::select(-Age) %>% mutate(Age = empt)
 train_NNA <- train_NNA %>% ungroup()
 train_L <- rbind(train_NNA,train_NA)
 
@@ -66,11 +67,27 @@ tr_shuffle <- tr[sample(1:nrow(tr)),]
 return(list(train = tr, test = te, shuffle = tr_shuffle))
 }
 
-#cross validation
+#different ML models to implement
+func_binom <- function(training,type,validation) {
+  if (type == "b") {
+    ML_output <- glm(factor(Survived) ~ ., data = training,family = binomial())
+    pred <- ifelse(predict(ML_output,newdata = validation, type = "response") > 0.5,1,0)
+  } else if (type == "l") {
+    ML_output <- lda(factor(Survived) ~., data = training)
+    pred <- predict(ML_output, newdata = validation)$class
+  } else if (type == "q") {
+    ML_output <- qda(factor(Survived) ~., data = training)
+    pred <- predict(ML_output, newdata = validation)$class
+  } else if (type == "B") {
+    ML_output <- naiveBayes(factor(Survived) ~., data = training)
+    pred <- predict(ML_output,newdata = validation)
+  }
+  return(pred)
+}
 
-cross_val <- function(k,tr_shuffle){
+
+cross_val <- function(k,tr_shuffle,type){
   #implementing k-fold CV
-  k <- 10
   fold_size <- floor(nrow(tr_shuffle)/k)
   class <- numeric(k)
   i <- 9
@@ -79,10 +96,9 @@ cross_val <- function(k,tr_shuffle){
     validation <- tr_shuffle[val_indices,]
     training <- tr_shuffle[-val_indices,]
     
-    logist_cv <- glm(factor(Survived) ~ ., data = training,family = binomial())
+    logist_cv <- func_binom(training,type,validation)
     
-    pred <- ifelse(predict(logist_cv,newdata = validation, type = "response") > 0.5,1,0)
-    pred <- factor(pred)
+    pred <- factor(logist_cv)
     test_pred <- factor(validation$Survived)
     conf <- confusionMatrix(pred,test_pred)
     class[i] <- conf$byClass[11]
@@ -93,45 +109,47 @@ cross_val <- function(k,tr_shuffle){
   return(class_avg)
 }
 
+
 #casewise deletion
 splitted <- split(train)
 shuffle <- as.data.frame(splitted$shuffle)
-(accuracy_casewise <- cross_val(10,shuffle))
+(accuracy_casewise <- cross_val(10,shuffle,"b"))
 
 #pairwise deletion
 split_pair <- split(train_na)
 shuffle_pair <- as.data.frame(split_pair$shuffle)
-(accuracy_pair <- cross_val(10,shuffle_pair))
+(accuracy_pair <- cross_val(10,shuffle_pair,"b"))
 
 #mean imputation
 split_mean <- split(train_mean)
 shuffle_mean <- as.data.frame(split_mean$shuffle)
-(accuracy_mean <- cross_val(10,shuffle_mean))
+(accuracy_mean <- cross_val(10,shuffle_mean,"b"))
 
 #regression imputation
 split_regress <- split(train_regress)
 shuffle_regress <- as.data.frame(split_regress$shuffle)
-(accuracy_regress <- cross_val(10,shuffle_regress))
+(accuracy_regress <- cross_val(10,shuffle_regress,"b"))
 
 #LOCF 
 split_LOCF <- split(train_L)
 shuffle_LOCF <- as.data.frame(split_LOCF$shuffle)
-(accuracy_LOCF <- cross_val(10,shuffle_LOCF))
+(accuracy_LOCF <- cross_val(10,shuffle_LOCF,"b"))
 
 
-#need to implement ML, expectation maximisation, multiple imputation and sensitivity analysis --
 
+
+#retrain model with highest accuracy
 
 
 #implement model with highest accuracy
 #y_tr <- factor(tr$Survived)
-#x_tr <- tr %>% select(-Survived)
+#x_tr <- tr %>% dplyr::select(-Survived)
 
 #logist <- glm(factor(Survived) ~ ., tr, family = binomial)
 #summary(logist)
 
 
-#x_te <- te %>% select(-Survived)
+#x_te <- te %>% dplyr::select(-Survived)
 #te_log <- predict(logist,x_te, type = "response")
 
 
